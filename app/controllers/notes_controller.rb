732657ca268e0343bb2d72c895b2b32962262b43
Evan <evanfujita@gmail.com>
2021-03-06 15:33:53 -0600

    models and crud. api functional

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,35 @@
 class NotesController < ApplicationController
+
+    def index
+        notes = Note.all
+        render json: notes
+    end
+
+    def show
+        note = Note.find_by(id: params[:id])
+        render json: note
+    end
+    
+    def new
+        note = Note.new(note_params)
+    end
+
+    def create
+        note = Note.new(note_params)
+        note.save
+        render json: note
+    end
+
+    def destroy
+        note = Note.find_by(id: params[:id])
+        note.destroy
+        render json: {message: 'deleted'}
+    end
+
+    private
+
+    def note_params
+        params.require(:note).permit(:title, :body, :user_id)
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,35 @@
 class UsersController < ApplicationController
 
     def index
-        user = User.find_by(id: params[:id])
-        render JSON: user
+        users = User.all
+        render json: users
     end
 
     def show
+        user = User.find_by(id: params[:id])
+        render json: user
+    end
+    
+    def new
+        user = User.new(user_params)
+    end
 
+    def create
+        user = User.new(user_params)
+        user.save
+        render json: user
     end
 
     def destroy
+        user = User.find_by(id: params[:id])
+        user.destroy
+        render json: {message: 'deleted'}
+    end
 
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
     end
 
 end
