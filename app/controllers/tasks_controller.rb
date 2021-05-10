7d94ecd6fa0ccbe55db09b3e02c8987a3deaf65b
Evan <evanfujita@gmail.com>
2021-05-10 16:26:01 -0500

    task controller set

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,45 @@
 class TasksController < ApplicationController
+    def index
+        tasks = Task.all
+        render json: tasks
+    end
+
+    def show
+        task = Task.find_by(id: params[:id])
+        render json: task
+    end
+    
+    def new
+        task = Task.new(task_params)
+    end
+
+    def create
+        task = Task.new(task_params(:title, :body, :user_id))
+        task.save
+        render json: task
+    end
+
+    def destroy
+        task = Task.find_by(id: params[:id])
+        task.destroy
+        render json: {message: 'deleted'}
+    end
+
+    def edit
+        task = Task.find_by(id: params[:id])
+    end
+
+    def update
+        task = Task.find_by(id: params[:id])
+        # byebug
+        task.update(task_params(:title, :body))
+        render json: task
+    end
+
+    private
+
+    def task_params(*args)
+        params.require(:task).permit(*args)
+    end
+
 end
